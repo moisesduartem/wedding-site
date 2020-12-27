48370dcdb9a7907158798fbe6921fe248d69685a
Moisés Mariano <moisesduartem@gmail.com>
2020-12-27 01:15:48 -0300

    fix: Header ~> custom-header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <Header />
+    <custom-header />
     <router-view/>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header';
+import CustomHeader from '@/components/Header';
 
 export default {
   name: 'App',
   components: {
-    Header
+    CustomHeader
   }
 }
 </script>

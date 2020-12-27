0a6900bb8d8b8c015e3d22d7d5a374f4da382872
Moisés Mariano <moisesduartem@gmail.com>
2020-12-27 01:16:15 -0300

    import & use message-list component

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="home">
+  <div>
+    <message-list />
   </div>
 </template>
 
 <script>
+import MessageList from '@/components/MessageList.vue'
 
 export default {
   name: 'Home',
   components: {
+    MessageList
   }
 }
 </script>
